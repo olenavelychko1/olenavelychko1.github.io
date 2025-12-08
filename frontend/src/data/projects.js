0cb029a0e4acc480e57a7a7559ceccd4cdd1895c
Lena Velychko <olenavelychko12@gmail.com>
2025-12-08 08:55:39 +0100

    feat: add blok 1 and 2 descriptions, adjust other descriptions

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -3,20 +3,20 @@ export const projects = [
     title: "Election Results Website",
     date: "September 2025 - now",
     // link: "https://github.com/olenavelychko1/blok-5-election-website",
-    technologies: ["html5", "css3", "java", "typescript", "vuejs", "springboot", "hibernate", "docker", "chartjs", "tailwindcss", "vite", "postman", "figma", "gitlab", "intellij"],
+    technologies: ["html5", "css3", "java", "typescript", "vuejs", "springboot", "hibernate", "docker", "chartjs", "tailwindcss", "postman", "figma", "gitlab", "intellij"],
     images: [
-      "/img/blok 5/elections-page1.jpeg",
       "/img/blok 5/elections-page2.jpeg",
+      "/img/blok 5/elections-page1.jpeg",
       "/img/blok 5/elections-page3.png",
       "/img/blok 5/elections-page4.jpeg"
     ],
-    description: `A full-stack web application that visualizes election results using responsive Vue components and a Spring Boot backend. I built the frontend with reusable, mobile-friendly components and implemented the backend with RESTful APIs for constituencies and results. I parsed a part of the official XML election data, mapped it into Java models, designed part of the database, and wrote unit tests. I'm currently expanding the platform with an admin dashboard to manage user actions on the forum and adding security features.`
+    description: `A full-stack web application that visualizes election results using responsive Vue components and a Spring Boot backend. I built the frontend with reusable, mobile-friendly components and implemented the backend with RESTful APIs for constituencies and results. I parsed a part of the official XML election data using transformers, mapped it into Java entity models with JPA/Hibernate, designed part of the database, and wrote unit tests. I'm currently expanding the platform with an admin dashboard to manage user actions on the forum and adding security features.`
   },
   {
     title: "Game Webshop Website",
     date: "April 2025 - July 2025",
     // link: "https://github.com/olenavelychko1/blok4-game-webshop",
-    technologies: ["html5", "css3", "typescript", "express", "vitest", "mysql", "vite", "esbuild", "docker", "postman", "figma", "gitlab", "vscode"],
+    technologies: ["html5", "css3", "typescript", "express", "vitest", "mysql", "docker", "postman", "figma", "gitlab", "vscode"],
     images: [
       "/img/blok 4/screenshot2.png",
       "/img/blok 4/screenshot4.png",
@@ -30,7 +30,7 @@ export const projects = [
     title: "Webgame: The Lost City of Azarath",
     date: "February 2025 - April 2025",
     // link: "#",
-    technologies: ["html5", "css3", "typescript", "figma", "esbuild", "vite", "express", "postman", "gitlab", "vscode"],
+    technologies: ["html5", "css3", "typescript", "figma", "express", "postman", "gitlab", "vscode"],
     images: [
       "/img/blok 3/screenshot1.png",
       "/img/blok 3/screenshot2.png",
@@ -48,7 +48,7 @@ export const projects = [
               <br />
               I personally contributed by developing the reading component, creating the dynamic sound system that
               changes room music and ambient sounds,
-              implementing the arrow navigation component, designing around 60% of the UI, building most of the game
+              implementing the arrow navigation component, designing most of the UI and characters, building many game
               rooms,
               and acting as the main bug fixer for the team. This project helped me significantly improve my TypeScript
               and HTML skills, communication within a team setting,
@@ -56,10 +56,10 @@ export const projects = [
             </div>`
   },
   {
-    title: "Programming Student Forum",
+    title: "Code Exchange - Programming Student Forum",
     date: "October 2024 - January 2024",
     // link: "#",
-    technologies: ["html5", "css3", "typescript", "nodejs", "mysql", "vite", "figma", "gitlab", "vscode",],
+    technologies: ["html5", "css3", "typescript", "mysql", "figma", "gitlab", "vscode",],
     images: [
       "/img/blok 2/screenshot1.png",
       "/img/blok 2/screenshot2.png",
@@ -67,13 +67,15 @@ export const projects = [
       "/img/blok 2/screenshot4.png",
       "/img/blok 2/screenshot5.png"
     ],
-    description: `hh`
+    description: `Code Exchange is a web-based programming student forum for our school, similar in concept to Stack Overflow, built as a duo project. The platform allows students to ask questions, share knowledge, and interact with peers. I primarily worked on the profile page, including an overview of user questions, the navbar, and the login page. 
+    <br/>
+    This was my first experience applying object-oriented programming and working with a database. I gained practical knowledge of SQL, table relationships, and ERDs while building a simple backend using CRUD operations and the MVC pattern. The main focus of the project was to apply OOP principles, connect the frontend with a MySQL database, and develop a real application while collaborating closely with a teammate. Through this project, I improved my TypeScript, HTML, and CSS skills and learned how to manage dynamic front-end components that interact with a backend.`
   },
   {
     title: "Dokkie - Outing Planner WebApp",
     date: "September 2024 - late October 2024",
     // link: "#",
-    technologies: ["html5", "css3", "typescript", "vite", "figma", "gitlab", "vscode",],
+    technologies: ["html5", "css3", "typescript", "figma", "gitlab", "vscode",],
     images: [
       "/img/blok 1/screenshot1.png",
       "/img/blok 1/screenshot2.png",
@@ -81,6 +83,7 @@ export const projects = [
       "/img/blok 1/screenshot4.png",
       "/img/blok 1/screenshot5.png"
     ],
-    description: `hh`
+    description: `Dokkie is a web-based outing planner that allows users to save outings, manage participants, and split costs. This was my first-year project, where I primarily focused on building the frontend using TypeScript, HTML, and CSS. Through this project, I learned the fundamentals of TypeScript, CSS and HTML, and gained practical experience with Git, including committing, pushing, and managing version control.
+`
   },
 ];
